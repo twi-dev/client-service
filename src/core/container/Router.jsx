77b3ed8fe1d413b4784d569d778c8e2909ed9e58
Nick K <nick.kruchinin@gmail.com>
2017-12-21 12:31:58 +0300

    Add a component for 404 error.

--- src/core/container/Router.jsx
+++ src/core/container/Router.jsx
@@ -2,11 +2,14 @@ import {h} from "preact"
 import {string, element, arrayOf, shape} from "prop-types"
 import {Switch, Route} from "react-router-dom"
 
+import NotFound from "core/view/error/NotFound"
+
 const Router = ({routes}) => (
   <Switch>
     {routes.map(props => <Route exact {...props} />)}
 
-    <Route component={() => <div>404</div>} />
+    {/* Render 404 error when no page found */}
+    <Route component={NotFound} />
   </Switch>
 )
 
---
+++ src/core/view/error/NotFound/NotFound.jsx
@@ -0,0 +1,7 @@
+import {h} from "preact"
+
+import {container} from "./not-found.sss"
+
+const NotFound = () => <div className={container}>404 | Page not found</div>
+
+export default NotFound
