8c39ea59cee3124d4af3547a4b5efbc03a6f4751
Nick K <nick.kruchinin@gmail.com>
2018-02-11 09:40:15 +0300

    Fix image scale on NotFound page.

--- src/core/view/error/NotFound/NotFound.jsx
+++ src/core/view/error/NotFound/NotFound.jsx
@@ -1,16 +1,18 @@
 import {h} from "preact"
 
-import {container} from "./not-found.sss"
+import {container, content, image} from "./not-found.sss"
 
 import Image from "./not-found.svg"
 
 const NotFound = () => (
   <div class={container}>
-    <div>
-      <Image />
-    </div>
-    <div>
-      Page Not Found
+    <div class={content}>
+      <div class={image}>
+        <Image />
+      </div>
+      <div>
+        Page Not Found
+      </div>
     </div>
   </div>
 )
