1e4644acc2682d5bc8cde8df267252b2738e6a83
Nick K <nick.kruchinin@gmail.com>
2018-02-11 09:45:43 +0300

    WTF git??

--- src/core/view/error/NotFound/NotFound.jsx
+++ src/core/view/error/NotFound/NotFound.jsx
@@ -1,7 +1,20 @@
 import {h} from "preact"
 
-import {container} from "./not-found.sss"
+import {container, content, image} from "./not-found.sss"
 
-const NotFound = () => <div className={container}>404 | Page not found</div>
+import Image from "./not-found.svg"
+
+const NotFound = () => (
+  <div class={container}>
+    <div class={content}>
+      <div class={image}>
+        <Image />
+      </div>
+      <div>
+        Page Not Found
+      </div>
+    </div>
+  </div>
+)
 
 export default NotFound
