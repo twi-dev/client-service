f3a85a19b6ae493e933906cbda26955506b231a8
Nick K <nick.kruchinin@gmail.com>
2018-03-30 12:13:42 +0300

    Rewrite objectIterator.

--- src/core/helper/iterator/objectIterator.js
+++ src/core/helper/iterator/objectIterator.js
@@ -1,15 +1,48 @@
-function* objectIterator(iterable, entries = false) {
-  if (!iterable) {
-    return
+class ObjectIterator {
+  constructor(iterable) {
+    this.__iterable = iterable
+
+    // I'm using this method because of fucking semicolon -_-
+    Array.from([Symbol.iterator, "keys", "values", "entries"])
+      .forEach(name => this[name] = this[name].bind(this))
   }
 
-  for (const key of Object.keys(iterable)) {
-    const value = iterable[key]
+  keys() {
+    if (!this.__iterable) {
+      return []
+    }
 
-    yield entries ? [key, value] : value
+    return Object.keys(this.__iterable)
+  }
+
+  * values() {
+    for (const [, value] of this.entries()) {
+      yield value
+    }
+  }
+
+  * entries() {
+    for (const key of this.keys()) {
+      const value = this.__iterable[key]
+
+      yield [key, value]
+    }
+  }
+
+  [Symbol.iterator]() {
+    return this.values()
   }
 }
 
-objectIterator.entries = iterable => objectIterator(iterable, true)
+const objectIterator = iterable => new ObjectIterator(iterable)
+
+const entries = iterable => new ObjectIterator(iterable).entries()
+
+const keys = iterable => new ObjectIterator(iterable).keys()
+
+const values = iterable => new ObjectIterator(iterable).values()
+
+objectIterator.entries = entries
 
 export default objectIterator
+export {ObjectIterator, entries, keys, values}
