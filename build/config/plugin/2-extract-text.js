c70b2711b7e6245ecad443fcde9ff7ad3f716f6f
Nick K <nick.kruchinin@gmail.com>
2018-04-08 23:19:15 +0300

    Bump deps. Fix issue with [contenthash] in extract-text plugin config.

--- build/config/plugin/2-extract-text.js
+++ build/config/plugin/2-extract-text.js
@@ -1,7 +1,7 @@
 const ExtractText = require("extract-text-webpack-plugin")
 
 const extractText = ({dev}) => new ExtractText({
-  filename: "css/[name]-[contenthash].css",
+  filename: "css/[name]-[hash].css",
   disable: dev,
   allChunks: true
 })
