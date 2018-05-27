69d8bfc2c0df0acf182b45705044b4025716dc38
Nick K <nick.kruchinin@gmail.com>
2018-05-28 00:34:55 +0300

    Add docs for preventDefault decorator.

--- src/core/helper/decorator/preventDefault.mjs
+++ src/core/helper/decorator/preventDefault.mjs
@@ -1,14 +1,34 @@
 import isFunction from "lodash/isFunction"
 
-const decorator = fn => function preventDefaultDecorator(event) {
-  if (!isFunction(fn)) {
-    fn = () => {}
+/**
+ * Decorate given event handler and return the new one which is calls
+ * event.peventDefault method before handler.
+ *
+ * @param {(event: Event) => void} handler
+ *
+ * @return {(event: Event) => void} preventDefaultDecorator
+ */
+const decorator = handler => function preventDefaultDecorator(event) {
+  if (!isFunction(handler)) {
+    handler = () => {}
   }
 
   event.preventDefault()
-  fn.call(this, event)
+  handler.call(this, event)
 }
 
+/**
+ * Decorator given event handler and call event.preventDefault() before it.
+ * You can decorator a function or class method.
+ *
+ * Note: if called with a function or without any arguments, it just returns
+ * a new event handler that calls preventDefault and then given function.
+ *
+ * @param {object | function} [target = undefined] – an event hanlder
+ *  or target object
+ * @param {string} [key = undefined] – property name
+ * @param {object} [descriptor = undefined] – a JavaScript property descriptor
+ */
 function preventDefault(target, key, descriptor) {
   if (isFunction(target) || arguments.length <= 1) {
     return decorator(target)
@@ -21,9 +41,9 @@ function preventDefault(target, key, descriptor) {
       return decorator(init.call(this))
     }
   } else {
-    const fn = descriptor.value
+    const handler = descriptor.value
 
-    descriptor.value = decorator(fn)
+    descriptor.value = decorator(handler)
   }
 
   return descriptor
