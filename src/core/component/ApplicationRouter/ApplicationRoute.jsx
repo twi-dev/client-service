28adab4e118dd0684390574c8ce91ce29bcc4ac3
Nick K <nick.kruchinin@gmail.com>
2018-11-15 20:31:47 +0300

    Rewrite router from scratch.

--- src/core/component/ApplicationRouter/ApplicationRoute.jsx
+++ src/core/component/ApplicationRouter/ApplicationRoute.jsx
@@ -1,28 +1,42 @@
-import React from "react"
-import Route from "react-router-dom/Route"
+import {join} from "path"
+
+import {createElement} from "react"
+import {oneOfType, func, string, bool} from "prop-types"
+import {Route as BaseRoute} from "react-router-dom"
 
 import DefaultLayout from "common/component/Layout"
 
-const ApplicationRoute = ({component: Target, layout: Layout, ...others}) => (
-  <Route
-    {...others}
+function ApplicationRoute(props) {
+  const {component: Target, layout: Layout, path, prefix, ...routeProps} = props
 
-    render={
-      props => do {
-        if (Layout === false) {
-          <Target {...props} />
-        } else if (!Layout) {
-          <DefaultLayout>
-            <Target {...props} />
-          </DefaultLayout>
-        } else {
-          <Layout>
-            <Target {...props} />
-          </Layout>
-        }
+  return createElement(BaseRoute, {
+    ...routeProps,
+
+    path: prefix ? join("/", prefix, path) : path,
+
+    render: targetProps => do {
+      if (Layout === false) {
+        createElement(Target, targetProps)
+      } else if (!Layout) {
+        createElement(DefaultLayout, null, createElement(Target, targetProps))
+      } else {
+        createElement(Layout, null, createElement(Target, targetProps))
       }
     }
-  />
-)
+  })
+}
+
+ApplicationRoute.propTypes = {
+  layout: oneOfType([func, bool]),
+  component: func.isRequired,
+  prefix: string,
+  exact: bool
+}
+
+ApplicationRoute.defaultProps = {
+  layout: null,
+  prefix: null,
+  exact: true
+}
 
 export default ApplicationRoute
