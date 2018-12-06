54df7d5b40b1eb63be159bf54f752e033ed02ae1
Nick K <nick.kruchinin@gmail.com>
2018-12-07 01:55:14 +0300

    Add common Form component

---
+++ src/common/component/Form/Form.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+import {func, shape, element, arrayOf, oneOfType} from "prop-types"
+
+import forwardRef from "core/hoc/forwardRef"
+import preventDefault from "core/helper/decorator/preventDefault"
+
+const Form = ({onSubmit, forwardedRef, children, ...props}) => (
+  <form {...props} onSubmit={preventDefault(onSubmit, forwardedRef)}>
+    {children}
+  </form>
+)
+
+Form.propTypes = {
+  onSubmit: func,
+  forwardedRef: shape({}),
+  children: oneOfType([element, arrayOf(element)]).isRequired
+}
+
+Form.defaultProps = {
+  onSubmit: null,
+  forwardedRef: null
+}
+
+export default Form |> forwardRef
