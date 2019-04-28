73670683776b6af7ceb3966a2e39938b2cca517f
Nick K <nick.kruchinin@gmail.com>
2019-04-28 23:36:33 +0300

    Remove default title from the application container template.

--- build/config/plugin/4-html.js
+++ build/config/plugin/4-html.js
@@ -1,7 +1,6 @@
 const Html = require("html-webpack-plugin")
 
 const html = () => new Html({
-  title: "Twi",
   template: "core/base/main.html",
   filename: "../index.html", // Realtive to "src" dir. (Dest path)
   inject: "body",
