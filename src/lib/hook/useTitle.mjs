7fa62eab94ea3659dbbe956a38fe50bbe15e44bb
octet-stream <nick.kruchinin@gmail.com>
2020-01-09 03:23:45 +0300

    Add docs for useTitle hook.

--- src/lib/hook/useTitle.mjs
+++ src/lib/hook/useTitle.mjs
@@ -4,12 +4,21 @@ import isString from "lodash/isString"
 const {isArray} = Array
 
 const defaults = {
-  prefix: undefined,
   delimiter: "–",
+  prefix: undefined,
   title: undefined,
   suffix: undefined
 }
 
+/**
+ * Enhances the `useTitle` hook from `react-use` library with optional params.
+ *
+ * @param {object | string} options – when set as string, will be used as title
+ * @param {string | array} [options.title = undefined] – a page title section(s)
+ * @param {string} [options.prefix = undefined] – title prefix
+ * @param {string} [options.suffix = undefined] – title suffix
+ * @param {string} [options.delimiter = "-"] – title sections delimiter
+ */
 function useTitle(options = {}) {
   if (isString(options)) {
     options = {title: options}
