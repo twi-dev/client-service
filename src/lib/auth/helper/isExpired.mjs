461c89d9ee5cb8d699e74b204653ae4ab84c53f0
octet-stream <nick.kruchinin@gmail.com>
2020-01-21 11:38:18 +0300

    Fix for isExpired auth helper and confirmation messages style.

--- src/lib/auth/helper/isExpired.mjs
+++ src/lib/auth/helper/isExpired.mjs
@@ -1,4 +1,6 @@
+import parseISO from "date-fns/parseISO"
 import getTime from "date-fns/getTime"
+import isString from "lodash/isString"
 
 /**
  * Check if given accessToken is expired.
@@ -12,7 +14,12 @@ function isExpired(accessToken) {
     return true
   }
 
-  return Boolean(Date.now() >= getTime(accessToken.expires))
+  let {expires} = accessToken
+  if (isString(expires)) {
+    expires = parseISO(expires)
+  }
+
+  return Boolean(Date.now() >= getTime(expires))
 }
 
 export default isExpired
