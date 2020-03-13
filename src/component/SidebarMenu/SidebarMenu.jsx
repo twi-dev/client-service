fb2a1416bb945d1f646a4caade6dd4b84cbb6680
octet-stream <nick.kruchinin@gmail.com>
2020-03-13 03:22:09 +0300

    Improve sidebar menu.

--- src/component/SidebarMenu/SidebarMenu.jsx
+++ src/component/SidebarMenu/SidebarMenu.jsx
@@ -1,13 +1,16 @@
 import {createElement} from "react"
 import {observer} from "mobx-react"
 
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faHome} from "@fortawesome/free-solid-svg-icons"
+
 import useStore from "lib/hook/useStore"
 
 import Link from "component/Link"
 import Avatar from "component/Avatar"
 import Viewer from "model/User/Viewer/Context"
 
-import {container, spacing} from "./sidebar-menu.css"
+import {container, item, spacing} from "./sidebar-menu.css"
 
 function SidebarMenu() {
   const {viewer, isSigned} = useStore(Viewer)
@@ -15,7 +18,9 @@ function SidebarMenu() {
   return (
     <div className={container}>
       <Link to="/" color="secondary">
-        Home
+        <div className={item}>
+          <FontAwesomeIcon icon={faHome} size="2x" />
+        </div>
       </Link>
 
       <div className={spacing} />
