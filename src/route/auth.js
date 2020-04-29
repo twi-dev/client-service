3b85c0a1db2c99fc927a89a736b85b693b544f11
octet-stream <nick.kruchinin@gmail.com>
2020-04-30 00:02:26 +0300

    Small fixes.

--- src/route/auth.js
+++ src/route/auth.js
@@ -9,7 +9,6 @@ import resolve from "lib/helper/util/requireDefault"
 
 import Context from "model/User/Viewer/Context"
 
-// TODO: Actually I'll better to render 403 error here instead
 const redirect = createRedirect({
   getUrl() {
     const {isSigned} = useContext(Context)
